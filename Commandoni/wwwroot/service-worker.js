8fde890a255c294b75eae68c5bf7a90c019335a8
Mehrdad Jannesar <Jannesar.computer@yahoo.com>
2026-07-27 23:52:33 +0330

    Improve dark hash bar contrast

--- Commandoni/wwwroot/service-worker.js
+++ Commandoni/wwwroot/service-worker.js
@@ -1,3 +1,3 @@
-self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open('commandoni-v3').then(cache=>cache.addAll(['/','/manifest.webmanifest','/css/site.css'])))});
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('commandoni-')&&key!=='commandoni-v3').map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open('commandoni-v4').then(cache=>cache.addAll(['/','/manifest.webmanifest','/css/site.css'])))});
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('commandoni-')&&key!=='commandoni-v4').map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method==='GET')event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));});
